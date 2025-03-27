f255b0493d9ec723ade203fea37eea9295cfcf71
Vinuuu5 <vinushreeselvam@gmail.com>
2025-03-27 10:56:38 +0530

    fitness

--- projectfit/Backend/Fitness-main/src/App.jsx
+++ projectfit/Backend/Fitness-main/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FitnessTracker from "./Fitness-Tracker";
-import Login from "./Login";
+import Login from "./login";
 import Signup from "./signup";
 
 function App() {
